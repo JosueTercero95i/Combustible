a68c70e92305a0d6b2b22ef191edd2d7e13823d5
JosueTercero95i <167829608+JosueTercero95i@users.noreply.github.com>
2024-05-12 17:59:25 -0600

    first commit

---
+++ COMBUSTIBLE/Automovil.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMBUSTIBLE
+{
+    public partial class Automovil
+    {
+        public string Marca { get; set; }
+        public string Placa { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+        public double CapacidadTanque { get; set; }
+        public double NivelCombustible { get; set; } 
+        public double Rendimiento { get; set; } 
+
+        public Automovil(string marca, string placa, string modelo, string color, double capacidadTanque, double Rendimiento)
+        {
+            this.Marca = marca;
+            this.Placa = placa;
+            this.Modelo = modelo;
+            this.Color = color;
+            this.CapacidadTanque = CapacidadTanque;
+            this.NivelCombustible = 0; 
+            this.Rendimiento = Rendimiento;
+        }
+
+        public void Drive(int distancia)
+        {
+            double CombustibleNecesario = distancia / Rendimiento; 
+            if (CombustibleNecesario <= NivelCombustible)
+            {
+                NivelCombustible -= CombustibleNecesario;
+                Console.WriteLine("puede conducir");
+            }
+            else
+            {
+                Console.WriteLine("combustible insuficiente");
+            }
+        }
+
+        public bool PuedeConducir(int distancia)
+        {
+            double combustibleNecesario = distancia / Rendimiento;
+            return combustibleNecesario <= NivelCombustible;
+        }
+}
+
+}
---
+++ COMBUSTIBLE/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace COMBUSTIBLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Automovil auto1 = new Automovil("Ford", "MY30527", "Scape", "Verde", 44, 12); // 12 km/galón
+            Automovil auto2 = new Automovil("Toyota", "MY78787", "Corolla", "Negro", 55, 10); // 10 km/galón
+
+            auto1.Drive(200); //km
+            auto2.Drive(300); //km
+
+            Console.WriteLine("¿Auto 1 puede conducir 600 km? " + (auto1.PuedeConducir(600) ? "Sí" : "No"));
+            Console.WriteLine("¿Auto 2 puede conducir 400 km? " + (auto2.PuedeConducir(400) ? "Sí" : "No"));
+
+
+  
+
+            Console.ReadLine();
+        }
+    }
+}
